92b434d148603775cc61a49ba4dbe8b39f6b9711
julesss-coder <julesss@posteo.de>
2024-03-11 15:58:47 +0100

    Implement endpoint for getting individual note using promise-based API

--- backend/server.js
+++ backend/server.js
@@ -13,31 +13,25 @@ app.use(express.json());
 
 // Using the Promise-based API: async/await
 app.get("/notes", async (request, response) => {
-    // When reading a JSON file from the filesystem in NodeJS using `fs.readFile()`, 
-    // the data you get is a string, not a JS object. Even though this string is in 
-    // JSON format, JS treats it as a plain string. So you cannot directly access the
-    // properties of this object.
-    // In order to access its properties, you need to convert the JSON string into a JS object
-    // using (JSON.parse()).
-    // When finished working with the JS object, you need to convert the JS object back to 
-    // a JSON string, as HTTP is a text-based protocol that deals with strings, not JS objects. 
-    // This conversion from JS to JSON is done by Express' response.json() method.
-    // So, the process is: read JSON string from file -> parse JSON string to JavaScript object -> manipulate data as needed -> convert JavaScript object back to JSON string -> send JSON string as HTTP response.
-    const data = await fs.readFile('data/notes.json', { encoding: 'utf-8' });
-    // `data` is already a JSON string, so I don't need to use `response.json()` to convert is to JSON before sending.
-    response.type('application/json');
-    response.send(data);
-    // response.json(JSON.parse(data));
-    
-
-    // fs.readFile('data/notes.json', 'utf8', (error, data) => {
-    //     if (error) {
-    //         console.error(error);
-    //         response.status(500).json({error: 'Error reading notes'});
-    //     } else {
-    //         response.json(JSON.parse(data));
-    //     }
-    // });
+    try {
+        // When reading a JSON file from the filesystem in NodeJS using `fs.readFile()`, 
+        // the data you get is a string, not a JS object. Even though this string is in 
+        // JSON format, JS treats it as a plain string. So you cannot directly access the
+        // properties of this object.
+        // In order to access its properties, you need to convert the JSON string into a JS object
+        // using (JSON.parse()).
+        // When finished working with the JS object, you need to convert the JS object back to 
+        // a JSON string, as HTTP is a text-based protocol that deals with strings, not JS objects. 
+        // This conversion from JS to JSON is done by Express' response.json() method.
+        // So, the process is: read JSON string from file -> parse JSON string to JavaScript object -> manipulate data as needed -> convert JavaScript object back to JSON string -> send JSON string as HTTP response.
+        const data = await fs.readFile('data/notes.json', { encoding: 'utf-8' });
+        // `data` is already a JSON string, so I don't need to use `response.json()` to convert is to JSON before sending.
+        response.type('application/json');
+        response.send(data);
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({error}); // TODO Is this the correct status code?
+    }
 });
 
 app.post("/notes", (request, response) => {
@@ -68,22 +62,48 @@ app.post("/notes", (request, response) => {
 });
 
 // GET a note with a specific ID
-app.get("/notes/:id", (request, response) => {
+app.get("/notes/:id", async (request, response) => {
     const { id } = request.params; 
-    fs.readFile('data/notes.json', 'utf8', (error, data) => {
-        if (error) {
-            console.error(error);
-            response.status(500).json({error: 'Error reading notes'});
-        } else {
-            const { notes } = JSON.parse(data);
-            if (notes[id]) {
-                response.json(notes[id]);
-            } else {
-                response.status(404).json({error: 'Note not found'});
-            }
-        }
-    });
+    let notes;
+
+    // get all notes from file
+    try {
+        notes = await fs.readFile('data/notes.json', { encoding: 'utf-8' });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({error: 'Error reading notes'});
+        return;
+    }
+    
+    // if note with this id exists, send it
+    notes = JSON.parse(notes);
+    try {
+        response.send(notes[id]);
+    // else, send error message
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({error: 'Error: Note not found'});
+        return;
+    }
 });
+
+
+// app.get("/notes/:id", (request, response) => {
+//     const { id } = request.params; 
+//     fs.readFile('data/notes.json', 'utf8', (error, data) => {
+//         if (error) {
+//             console.error(error);
+//             response.status(500).json({error: 'Error reading notes'});
+//         } else {
+//             const { notes } = JSON.parse(data);
+//             if (notes[id]) {
+//                 response.json(notes[id]);
+//             } else {
+//                 response.status(404).json({error: 'Note not found'});
+//             }
+//         }
+//     });
+// });
 
 // What happens with GET request to /new?
 app.get("/new", (request, response) => {
